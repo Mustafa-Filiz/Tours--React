81ba74aedce5dac5ed44feadb444d37bee2c63d1
Mustafa Filiz <mustafafiliz18@gmail.com>
2021-09-16 23:28:56 +0300

    updates and read more button added

--- src/Tour.js
+++ src/Tour.js
@@ -1,6 +1,12 @@
 import React, { useState } from 'react';
 
 const Tour = ({ tour }) => {
+    const [lessInfo, setLessInfo] = useState(true);
+
+    // const handleInfo = () => {
+    //     return lessInfo === tour.info.slice(0, 200) ? setLessInfo(tour.info) : setLessInfo(tour.info.slice(0, 200))
+    // }
+
     return (
         <article className="single-tour">
             <img src={tour.image} alt={tour.name} />
@@ -9,7 +15,19 @@ const Tour = ({ tour }) => {
                     <h4>{tour.name}</h4>
                     <h4 className="tour-price">${tour.price}</h4>
                 </div>
-                <p>{tour.info}</p>
+                <p>
+                    {lessInfo ? `${tour.info.slice(0, 200)}...` : tour.info}
+                    {lessInfo ? (
+                        <button onClick={() => setLessInfo(!lessInfo)}>
+                            Read More
+                        </button>
+                    ) : (
+                        <button onClick={() => setLessInfo(!lessInfo)}>
+                            Show Less
+                        </button>
+                    )}{' '}
+                </p>
+
                 <button className="delete-btn">not interested</button>
             </footer>
         </article>
